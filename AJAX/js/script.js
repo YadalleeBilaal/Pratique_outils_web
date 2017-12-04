141121183b657389fa1434bc314ffb20143b1382
yadallee <bilaal.yadallee@gmail.com>
2017-12-04 23:37:38 +0100

    Utilisation du template Handlebars + conditionnement

--- AJAX/js/script.js
+++ AJAX/js/script.js
@@ -8,9 +8,7 @@
 
 // request.send();
 var xmlHttp = new XMLHttpRequest();
-var div = document.getElementById("result")
 var textBar = document.getElementsByTagName("input");
-div.style.display = "none";
 function detectInput()
 {
   if(textBar[0].value.length > 0)
@@ -19,18 +17,24 @@ function detectInput()
 	xmlHttp.onreadystatechange = function(){
 		if(this.readyState == 4 && this.status == 200)
 		{
-			var object = this.responseText;
-			console.log(object);
-			div.innerHTML = object;
-			div.style.display = "block";
+			var templateScript = document.getElementById("handle").innerHTML;
+			var templateCompilation = Handlebars.compile(templateScript);
+			if(this.responseText.indexOf("None") < 0)
+			{
+				var object = templateCompilation(JSON.parse(this.responseText));
+				//console.log(JSON.parse(this.responseText));
+				document.getElementById("result").innerHTML = "<br/>"+object;
+			}
+			else
+			{
+				document.getElementById("result").innerHTML = "<p id='error'>Nom introuvable !</p>";
+			}
+			
+			//onsole.log(object);
 		}
 	};
-	xmlHttp.open("GET","http://localhost/AJAX_JSON/launch.php?page=control&function=runQuery&arg="+textBar[0].value,true);
+	xmlHttp.open("GET","http://localhost/AJAX_JSON/AJAX/launch.php?page=control&function=runQuery&arg="+textBar[0].value,true);
 	xmlHttp.send(null);
 
-  }
-  else
-  {
-  	div.style.display = "none";
   }
 }
