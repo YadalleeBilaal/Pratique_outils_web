74fedab7542e825e9cee2cf6ec2b8e2caca72881
yadallee <bilaal.yadallee@gmail.com>
2017-12-04 23:38:49 +0100

    Correction de PDO + optimisation du tableau pour JSON

--- AJAX/search.php
+++ AJAX/search.php
@@ -3,12 +3,28 @@
 <head>
 	<title>AJAX and JSON</title>
 	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 	<h2>Application de recherche dans une base de données</h2>
 	<label for="looking_for">Saisir quelque chose :</label>
 	<input type="text" name="looking_for" oninput="detectInput()"/>
+	<script id="handle" type="text/x-handlebars-template">
+		<table id="result">
+			<tr>
+				<th>Nom</th>
+				<th>Identifiant</th>
+			</tr>
+		{{#each this}}
+			<tr>
+				<td>{{name}}</td>
+				<td>{{id}}</td>
+			</tr>
+		{{/each}}
+		</table>
+	</script>
 	<div id="result"></div>
+	<script src="js/handlebars.js"></script>
 	<script src="js/script.js"></script>
 </body>
 </html>
